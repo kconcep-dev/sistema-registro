14c696d514cc2e2f988157283470d204d0cfd2b7
Kevin Rodríguez <proyectofinal690@gmail.com>
2025-08-31 16:24:50 -0500

    Actualización del código

--- script.js
+++ script.js
@@ -192,7 +192,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 // Peticiones avanzadas al navegador
                 advanced: [
                     { torch: true },         // Pedir que encienda la linterna
-                    { zoom: 2.0 },            // Pedir un zoom de 2x
+                    { zoom: 3.0 },            // Pedir un zoom de 2x
                     { focusMode: 'continuous' } // Pedir que intente enfocar continuamente
                 ]
             }
