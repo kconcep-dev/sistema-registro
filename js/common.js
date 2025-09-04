4622449acbca9e677851859913ab5355ca1ed33e
Kevin Rodríguez <proyectofinal690@gmail.com>
2025-09-03 22:25:27 -0500

    Actualización del código

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@
 
 // --- 1. CONFIGURACIÓN Y CLIENTE SUPABASE ---
 const supabaseUrl = "https://qmzbqwwndsdsmdkrimwb.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzIΙNiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtemJxd3duZHNkc21ka3JpbXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0OTExNDYsImV4cCI6MjA3MjA2NzE0Nn0.dfQdvfFbgXdun1kQ10gRsqh3treJRzOKdbkebpEQXWo";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtemJxd3duZHNkc21ka3JpbXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0OTExNDYsImV4cCI6MjA3MjA2NzE0Nn0.dfQdvfFbgXdun1kQ10gRsqh3treJRzOKdbkebpEQXWo";
 const supabaseClient = supabase.createClient(supabaseUrl, supabaseKey);
 
 // --- 2. INYECTOR DE BARRA DE NAVEGACIÓN (CENTRALIZADO) ---
