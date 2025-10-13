cf9d11d4afa39a849f968c71cea048b61240676c
Kevin Rodriguez <proyectofinal690@gmail.com>
2025-10-12 21:07:52 -0500

    ACTUALIZACIÓN

--- js/config.example.js
+++
@@ -1,6 +0,0 @@
-window.__SUPABASE_CONFIG__ = {
-  // URL del proyecto Supabase (por ejemplo: "https://xyzcompany.supabase.co")
-  url: 'https://TU-PROYECTO.supabase.co',
-  // Clave pública (anon) del proyecto
-  key: 'TU-CLAVE-ANON-PUBLICA'
-};
--- js/config.js
+++
@@ -1,4 +0,0 @@
-window.__SUPABASE_CONFIG__ = {
-  url: 'https://qmzbqwwndsdsmdkrimwb.supabase.co',
-  key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFtemJxd3duZHNkc21ka3JpbXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTY0OTExNDYsImV4cCI6MjA3MjA2NzE0Nn0.dfQdvfFbgXdun1kQ10gRsqh3treJRzOKdbkebpEQXWo'
-};
