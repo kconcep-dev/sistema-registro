a366152c4b0a7a8b9a96f6b96b3fea346396fdfd
kconcep-dev <proyectofinal690@gmail.com>
2025-10-12 21:38:00 -0500

    Add basic PWA support

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = 'sistema-registro-shell-v1';
+const CORE_ASSETS = [
+  './',
+  './inicio.html'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS)).catch(() => Promise.resolve())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames
+          .filter((name) => name !== CACHE_NAME)
+          .map((name) => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+
+      return fetch(event.request).catch((error) => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./inicio.html');
+        }
+        throw error;
+      });
+    })
+  );
+});
